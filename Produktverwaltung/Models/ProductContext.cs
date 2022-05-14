d3c47d5dd5b199a828f8877ace7a158cbc63ac5d
mka-99 <michael.kaufmann@edu.campus02.at>
2022-05-15 00:40:59 +0200

    e-learning2

---
+++ Produktverwaltung/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Produktverwaltung.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Product> TodoItems { get; set; } = null!;
+    }
+}
